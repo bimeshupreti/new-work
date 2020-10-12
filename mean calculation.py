589c1bd1229c17c72ab0ccb67dec4be7b0ad704e
bimeshupreti <bimesh.upreti@gmail.com>
2020-10-12 10:27:19 +0300

    new number added

--- mean calculation.py
+++ mean calculation.py
@@ -4,7 +4,7 @@
 # In[1]:
 
 
-my_list = [1, 2, 3 , 4, 5, 6, 7, 8, 9, 10, 11, 12, 20]
+my_list = [1, 2, 3 , 4, 5, 6, 7, 8, 9, 10, 11, 12, 20, 22]
 average = sum(my_list)/len(my_list)
 print("the mean is" + str(round(average, 2)))
 
