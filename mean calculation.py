0cc909f7a634860dc423ca56992bfe4fdda19265
bimeshupreti <bimesh.upreti@gmail.com>
2020-10-12 12:11:40 +0300

    number 40 added

--- mean calculation.py
+++ mean calculation.py
@@ -2,7 +2,7 @@
 
 
 
-my_list = [1, 2, 3 , 4, 5, 6, 7, 8, 9, 10, 11, 12, 15, 25, 30, 35 ]
+my_list = [1, 2, 3 , 4, 5, 6, 7, 8, 9, 10, 11, 12, 15, 25, 30, 35, 40]
 average = sum(my_list)/len(my_list)
 print("the mean is" + str(round(average, 2)))
 
