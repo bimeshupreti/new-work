22fbf3daa19fbea05e51c8a9d9760f43fd4a8502
bimeshupreti <bimesh.upreti@gmail.com>
2020-10-12 09:52:46 +0300

    first commit

---
+++ mean calculation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+my_list = [1, 2, 3 , 4, 5, 6, 7, 8, 9, 10]
+average = sum(my_list)/len(my_list)
+print("the mean is" + str(round(average, 2)))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
